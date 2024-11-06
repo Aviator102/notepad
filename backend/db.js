c2cf881190e2e2d108dc69ca90afdb23c7a090d0
Sonhador Brasil <magazinegravatai@gmail.com>
2024-11-06 01:26:58 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,10 @@
-const { Client } = require('pg');
-const dotenv = require('dotenv');
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// Carregar variáveis de ambiente
-dotenv.config();
-
-// Conectar ao banco de dados PostgreSQL
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+// Configuração do pool de conexões com o banco de dados
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-client.connect();
-
-// Função para criar uma nova nota
-const createNote = async (slug, content) => {
-    const query = 'INSERT INTO notes(slug, content) VALUES($1, $2) RETURNING *';
-    const values = [slug, content];
-    const res = await client.query(query, values);
-    return res.rows[0];
-};
-
-module.exports = { createNote };
+module.exports = pool;
