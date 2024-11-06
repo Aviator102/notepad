ad7f9b9422187fc2d669020b365915cb4a531f0c
Sonhador Brasil <magazinegravatai@gmail.com>
2024-11-06 00:53:45 -0300

    Update noteModel.js

--- backend/noteModel.js
+++ backend/noteModel.js
@@ -1,4 +1,6 @@
 // backend/noteModel.js
+const db = require('./db');
+
 async function createNoteTable() {
   const query = `
     CREATE TABLE IF NOT EXISTS notes (
@@ -8,10 +10,18 @@ async function createNoteTable() {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `;
-  try {
-    await db.query(query);
-    console.log('Tabela de notas criada/verificada com sucesso.');
-  } catch (error) {
-    console.error('Erro ao criar/verificar a tabela:', error);
-  }
+  await db.query(query);
 }
+
+async function createNote(slug, content) {
+  const query = `
+    INSERT INTO notes (slug, content) 
+    VALUES ($1, $2) 
+    RETURNING id, slug, content, created_at;
+  `;
+  const values = [slug, content];
+  const result = await db.query(query, values);
+  return result.rows[0];
+}
+
+module.exports = { createNote, createNoteTable };
